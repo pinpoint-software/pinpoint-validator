79c1065e69e40996634a5322744dc8c9ffac7b0f
Andrew Shell <andrew@andrewshell.org>
2014-08-09 09:21:24 -0500

    Better date behavior

--- src/Pinpoint/Validator/Sanitizer/Date.php
+++ src/Pinpoint/Validator/Sanitizer/Date.php
@@ -14,6 +14,15 @@ class Date implements Sanitizer
 
     public function sanitize($value)
     {
-        return date($this->format, strtotime($value));
+        if (is_string($value)) {
+            $ts = strtotime($value);
+        } else {
+            $ts = 0;
+        }
+        if (0 == $ts) {
+            return null;
+        } else {
+            return date($this->format, $ts);
+        }
     }
 }
