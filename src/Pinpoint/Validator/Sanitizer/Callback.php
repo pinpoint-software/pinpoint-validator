0d46cab911c2c0b14f48b5d7d19c6ae36a97254d
Andrew Shell <andrew@andrewshell.org>
2014-08-09 09:32:35 -0500

    Added Callback sanitizer

---
+++ src/Pinpoint/Validator/Sanitizer/Callback.php
@@ -0,0 +1,23 @@
+<?php
+namespace Pinpoint\Validator\Sanitizer;
+
+class Date implements Sanitizer
+{
+    public function __construct($params)
+    {
+        if (isset($params[0])) {
+            $this->callback = $params[0];
+        } else {
+            $this->callback = false;
+        }
+    }
+
+    public function sanitize($value)
+    {
+        if (is_callable($this->callback)) {
+            return call_user_func($this->callback, $value);
+        } else {
+            return null;
+        }
+    }
+}
